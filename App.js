c0e9cdb80ed025f3d8c56048fe4e2a57d31df06b
batuhanunverr <batuhannunverr@gmail.com>
2019-12-10 01:26:48 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React,{Component} from 'react';
+import Main from './app/components/Main';
+import Note from './app/components/Note';
+
+
+export default class App extends React.Component{
+render(){
+return(
+
+<Main/>
+
+);
+}
+
+}
